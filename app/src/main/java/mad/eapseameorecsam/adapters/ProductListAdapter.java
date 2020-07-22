75279817a3eff5153d61bc0fbfab91250c51e863
Seameorecsam Eap <recsam@Seameorecsams-MacBook-Pro.local>
2020-07-22 15:38:12 +0700

    Add products list adapter

--- app/src/main/java/mad/eapseameorecsam/adapters/ProductListAdapter.java
+++ app/src/main/java/mad/eapseameorecsam/adapters/ProductListAdapter.java
@@ -1,7 +1,62 @@
 package mad.eapseameorecsam.adapters;
 
-public class ProductListAdapter {
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
-//    static class ProductHolder extends
+import com.facebook.drawee.view.SimpleDraweeView;
+
+import java.util.List;
+
+import mad.eapseameorecsam.R;
+import mad.eapseameorecsam.models.Product;
+
+public class ProductListAdapter extends RecyclerView.Adapter<ProductListAdapter.ProductHolder> {
+
+    private List<Product> products;
+
+    public ProductListAdapter(List<Product> products) {
+        this.products = products;
+    }
+
+    @NonNull
+    @Override
+    public ProductHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ProductHolder(
+                LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.item_product, parent, false)
+        );
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProductHolder holder, int position) {
+        Product product = products.get(position);
+
+        holder.productName.setText(product.getName());
+        holder.imgPreview.setImageURI(Uri.parse(product.getImageUrl()));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return products.size();
+    }
+
+    static class ProductHolder extends RecyclerView.ViewHolder {
+        SimpleDraweeView imgPreview;
+        TextView productName;
+
+        public ProductHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imgPreview = itemView.findViewById(R.id.img_preview);
+            productName = itemView.findViewById(R.id.product_name);
+        }
+    }
+
 }
---
+++ app/src/main/java/mad/eapseameorecsam/models/Product.java
@@ -0,0 +1,107 @@
+package mad.eapseameorecsam.models;
+
+public class Product {
+
+    private int id;
+    private String name;
+    private String code;
+    private String description;
+    private double price;
+    private String imageUrl;
+    private long totalRate;
+    private double averageRate;
+    private long totalView;
+    private int categoryId;
+    private String categoryName;
+
+    public Product() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public long getTotalRate() {
+        return totalRate;
+    }
+
+    public void setTotalRate(long totalRate) {
+        this.totalRate = totalRate;
+    }
+
+    public double getAverageRate() {
+        return averageRate;
+    }
+
+    public void setAverageRate(double averageRate) {
+        this.averageRate = averageRate;
+    }
+
+    public long getTotalView() {
+        return totalView;
+    }
+
+    public void setTotalView(long totalView) {
+        this.totalView = totalView;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+}
