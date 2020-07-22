018a308c7b9aef6e556b050f6aef1eeafe722e36
recsam <eapseameorecsam@gmail.com>
2020-07-22 17:41:33 +0700

    Product Detail Activity

--- app/src/main/java/mad/eapseameorecsam/ui/ProductDetailActivity.java
+++ app/src/main/java/mad/eapseameorecsam/ui/ProductDetailActivity.java
@@ -3,8 +3,10 @@ package mad.eapseameorecsam.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -12,13 +14,11 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.facebook.drawee.view.SimpleDraweeView;
 import com.google.gson.Gson;
 
-import java.util.Arrays;
-import java.util.List;
 
 import mad.eapseameorecsam.R;
-import mad.eapseameorecsam.adapters.ProductListAdapter;
 import mad.eapseameorecsam.models.Product;
 
 public class ProductDetailActivity extends AppCompatActivity {
@@ -59,7 +59,21 @@ public class ProductDetailActivity extends AppCompatActivity {
     }
 
     private void updateUI() {
+        SimpleDraweeView img = findViewById(R.id.img);
+        TextView name = findViewById(R.id.product_name);
+        TextView code = findViewById(R.id.txt_code);
+        TextView description = findViewById(R.id.txt_desc);
+        TextView price = findViewById(R.id.txt_price);
+        TextView totalRate = findViewById(R.id.total_rate);
+        TextView averageRate = findViewById(R.id.average_rate);
 
+        img.setImageURI(Uri.parse(product.getImageUrl()));
+        name.setText(product.getName());
+        code.setText(product.getCode());
+        description.setText(product.getDescription());
+        price.setText("" + product.getPrice() + "$");
+        totalRate.setText("( " + product.getTotalRate() + " in total )");
+        averageRate.setText("" +product.getAverageRate());
     }
 
 }
